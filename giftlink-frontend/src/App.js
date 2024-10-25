ccecee71770880356da21a7c7104dc53607b6d62
camy2015 <cegor79@gmail.com>
2024-10-25 11:46:04 -0400

    Create Register and Login Pages

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import LoginPage from './components/LoginPage/LoginPage';
+import MainPage from './components/MainPage/MainPage';
 import Navbar from './components/Navbar/Navbar';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 
 function App() {
 
-  return (
-    <>
-        <Navbar/>
-        <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-        </Routes>
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                {/* the final code will not pass the products to every page, but each page will call the server API */}
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+            </Routes>
         </>
-  );
+    );
 }
 
 export default App;
