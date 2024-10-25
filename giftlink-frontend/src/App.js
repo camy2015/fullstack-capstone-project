be3a59926438aae2757ec1bbe1316c050d32c168
camy2015 <cegor79@gmail.com>
2024-10-25 12:35:04 -0400

    Implement the Details Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import MainPage from './components/MainPage/MainPage';
 import Navbar from './components/Navbar/Navbar';
@@ -18,6 +19,7 @@ function App() {
                 <Route path="/app" element={<MainPage />} />
                 <Route path="/app/login" element={<LoginPage />} />
                 <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
             </Routes>
         </>
     );
