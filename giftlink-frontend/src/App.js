1ca90b8ddb3cbd6f3d1ed5d0c054ac949310fc60
camy2015 <cegor79@gmail.com>
2024-10-25 13:20:32 -0400

    Implement the Search Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,7 @@ import LoginPage from './components/LoginPage/LoginPage';
 import MainPage from './components/MainPage/MainPage';
 import Navbar from './components/Navbar/Navbar';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/app/login" element={<LoginPage />} />
                 <Route path="/app/register" element={<RegisterPage />} />
                 <Route path="/app/product/:productId" element={<DetailsPage />} />
+                <Route path="/app/search" element={<SearchPage />} />
             </Routes>
         </>
     );
