8bc94311d2d725361c2940ccc0f7377ca5ae8835
Cesar Gonzalez <cegor79@gmail.com>
2024-10-28 19:25:18 -0500

    Implement Back-end in Profile Component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,30 +1,31 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import './App.css';
-import DetailsPage from './components/DetailsPage/DetailsPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import MainPage from './components/MainPage/MainPage';
-import Navbar from './components/Navbar/Navbar';
-import RegisterPage from './components/RegisterPage/RegisterPage';
-import SearchPage from './components/SearchPage/SearchPage';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import MainPage from "./components/MainPage/MainPage";
+import Navbar from "./components/Navbar/Navbar";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+import SearchPage from "./components/SearchPage/SearchPage";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-
-    return (
-        <>
-            <Navbar />
-            <Routes>
-                {/* the final code will not pass the products to every page, but each page will call the server API */}
-                <Route path="/" element={<MainPage />} />
-                <Route path="/app" element={<MainPage />} />
-                <Route path="/app/login" element={<LoginPage />} />
-                <Route path="/app/register" element={<RegisterPage />} />
-                <Route path="/app/product/:productId" element={<DetailsPage />} />
-                <Route path="/app/search" element={<SearchPage />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        {/* the final code will not pass the products to every page, but each page will call the server API */}
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
